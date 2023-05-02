8b7ddc268cb229659ffe15b4dbadc657fd74d1d7
MOSTA3D <khabbieny123@gmail.com>
2023-05-02 11:48:19 +0300

    [bugfix] depending on state for handling logic, not the data iteself

--- cegebest_spa/src/components/SearchBox.tsx
+++ cegebest_spa/src/components/SearchBox.tsx
@@ -12,25 +12,21 @@ import {
 import SearchList from "./SearchList";
 
 import { ApiMessage, ISearchBoxProps, PopularMovie } from "../utils/types";
-import { FetchFacad } from "../utils/helper";
+import { FetchFacad, getData } from "../utils/helper";
 import { BACKEND_URL } from "../utils/config";
 import { useNavigate } from "react-router";
 
 function SearchBox() {
   const [searchValue, setSearchValue] = useState("");
-  // const searchList = useRef([] as unknown as MovieType[]);
   const [searchList, setSearchList] = useState([] as PopularMovie[]);
   const [isBlured, setIsBlured] = useState(true);
 
   const navigator = useNavigate();
 
-  // handlers
   const handleInputBlur = (e: any) => {
     if (e.relatedTarget && e.relatedTarget.nodeName === "A") {
-      console.log(e.relatedTarget);
       navigator(e.relatedTarget.pathname);
       setIsBlured(true);
-      // navigator(e.relatedTarget.)
       return;
     }
     setIsBlured(true);
@@ -43,21 +39,16 @@ function SearchBox() {
   const handleSearchChange = (e: FormEvent<HTMLInputElement>): void => {
     const value = e.currentTarget.value;
     if (value.length > 3) {
-      const fetchFacad = FetchFacad.getFetchFacad();
-      // get request to the backend to search from the movie and render the SearchList component
-
       (async function () {
-        const data = await fetchFacad.getData<PopularMovie[]>(
-          `${BACKEND_URL}/search?query=${searchValue}`
-        );
-        if ((data as ApiMessage).message) {
-          return;
+        try {
+          const data = await getData<PopularMovie[]>(
+            `${BACKEND_URL}/search?query=${value}`
+          );
+          setSearchList(data);
+        } catch (e) {
+          console.log(e);
         }
-        setSearchList(data as PopularMovie[]);
-        console.log("triggered");
       })();
-
-      // dispatch the changes
     }
     setSearchValue(value);
   };
